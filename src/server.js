bce552a8dba4caff9b2066b08ce4ae1b014c43b8
Shubham Prakash <prakashshubham26@gmail.com>
2025-07-02 13:07:34 +0530

    Added Cron Job

--- src/server.js
+++ src/server.js
@@ -3,20 +3,24 @@ import dotenv from 'dotenv';
 import { initDB} from './config/db.js';
 import rateLimiter from './middleware/rateLimiter.js';
 import transactionsRoute from './routes/transactionsRoute.js';
+import job from './config/cron.js';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
 
-
+if(process.env.NODE_ENV === 'production') {
+    job.start();    // Start the cron job if in production environment
+    console.log("Cron job started");
+}
 
 //Middleware
 app.use(express.json());
 app.use(rateLimiter);
 
 // Routes
-app.get("/health", (req, res) => {
+app.get("/api/health", (req, res) => {
     res.status(200).json({ message: "Server is healthy" });
 });
 
